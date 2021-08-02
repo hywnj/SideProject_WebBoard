adb034ebbf31acb7d5ee1efa062995480796c870
hywnj <jhjsjym@gmail.com>
2021-08-02 21:04:41 +0900

    pw modify

--- common/db.php
+++ common/db.php
@@ -1,7 +1,7 @@
 <?php
     #echo "MySqli <br>" . date("Y-m-d H:i:s", time());
     global $db;
-    $db = mysqli_connect("localhost", "root", "970418", "board",  3306);
+    $db = mysqli_connect("localhost", "root", "(비밀번호)", "board",  3306);
     if($db){
         echo "connect : success<br>";
         error_log (date("Y-m-d H:i:s", time()) . " - mysqli_connect : success\n", 3, "C:/APM/Apache24/htdocs/debug.log");
