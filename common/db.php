2ee4e2c1cd4ef273272ca8cd8ac7ba9874e3eac0
hywnj <jhjsjym@gmail.com>
2022-05-16 20:18:34 +0900

    DB config 적용

--- common/db.php
+++ common/db.php
@@ -1,12 +1,14 @@
 <?php
-    #echo "MySqli <br>" . date("Y-m-d H:i:s", time());
-    global $db;
-    $db = mysqli_connect("localhost", "root", "(비밀번호)", "board",  3306);
-    if($db){
-        echo "connect : success<br>";
-        error_log (date("Y-m-d H:i:s", time()) . " - mysqli_connect : success\n", 3, "C:/APM/Apache24/htdocs/debug.log");
-    }
-    else{
-        echo "disconnect : fail<br>";
-        error_log (date("Y-m-d H:i:s", time()) . " - mysqli_connect : fail\n", 3, "C:/APM/Apache24/htdocs/debug.log");
-    }
+include_once $_SERVER['DOCUMENT_ROOT'] . '/common/config.php';
+
+#echo "MySqli <br>" . date("Y-m-d H:i:s", time());
+global $db;
+$db = mysqli_connect($DB_USER, $DB_ID, $DB_PW, $DB_NAME, $DB_PORT);
+if($db){
+    echo "connect : success<br>";
+    error_log (date("Y-m-d H:i:s", time()) . " - mysqli_connect : success\n", 3, "C:/APM/Apache24/htdocs/debug.log");
+}
+else{
+    echo "disconnect : fail<br>";
+    error_log (date("Y-m-d H:i:s", time()) . " - mysqli_connect : fail\n", 3, "C:/APM/Apache24/htdocs/debug.log");
+}
